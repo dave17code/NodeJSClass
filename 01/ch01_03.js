19162cb4c8528f6d25722b91198a2a4bc521efa4
dave17code <dave17code@gmail.com>
2025-04-09 14:14:58 +0900

    250409NodeJS오후수업시작

---
+++ 01/ch01_03.js
@@ -0,0 +1,16 @@
+console.log(typeof 10);
+console.log(typeof "hello"); // string
+console.log(typeof true); // boolean
+console.log(typeof function () {}); // function == 함수
+console.log(typeof {}); // object == 객체
+
+// 1. 숫자 52를 문자열로 변환하고 싶을 때 어떤 함수를 사용할 수 있을까요?
+// 2. true 값을 숫자로 바꾸면 어떤 숫자가 될까요?
+// 3. 문자 "hello"를 Number()로 변환하면 어떤 결과가 나올까요?
+// 4. 문자 "hello"를 숫자로 바꿨을 때 유효한 숫자인지 확인하려면 어떤 함수를 써야 할까요?
+// 5. 다음 중 Boolean()으로 변환했을 때 false가 되는 값을 모두 고르세요. -> 0,1,NaN, 빈 문자열 "", "hello"
+// 6. 문자열 "25"와 숫자 125를 더하면 결과는 어떻게 될까요?
+// 7. 문자열 "25"에서 숫자 125를 빼면 결과는 어떻게 될까요?
+// 8. typeof 연산자를 사용했을 때 함수는 어떤 타입으로 나올까요?
+// 9. 객체 리터럴 {}의 타입은 무엇으로 나올까요?
+// 10. undefined를 Number()로 변환하면 어떤 결과가 나올까요?
